a4c42f111ca88c62d7247fa3abd14980de427738
Jason Forrester <jason4ester1@gmail.com>
2024-08-16 11:47:44 -0500

    309.5.8 submission

---
+++ Labsv4/src/main/java/com/project/Labsv4/Labsv4Application.java
@@ -0,0 +1,13 @@
+package com.project.Labsv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labsv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labsv4Application.class, args);
+	}
+
+}
---
+++ Labsv4/src/test/java/com/project/Labsv4/Labsv4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Labsv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labsv4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
